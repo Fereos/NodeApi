1e648fefea834a666d1edc96a7a1b2bec7df7c0c
Fereos <code.melchor.pineda@gmail.com>
2021-02-21 12:39:37 +0800

    port change

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
 
-app.listen(3000, () => console.log('Server Started'))
+app.listen(process.env.PORT, () => console.log('Server Started'))
