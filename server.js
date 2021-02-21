ce7a2b46a37895c3fc9ba2910fbffd9a41e30b0f
Fereos <code.melchor.pineda@gmail.com>
2021-02-21 12:31:25 +0800

    change name for heroku db connectionnstring variable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 
 
-mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 const db = mongoose.connection
 
 db.on('error' , (error) => console.error(error))
